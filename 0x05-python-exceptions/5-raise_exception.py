85581ad91fecf6bd2b50d3dfbd51749eb92f8391
Glo-ry <glory0chiobi0@gmail.com>
2023-06-26 09:24:20 -0700

    Raise exception

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python3
+
+
 def raise_exception():
     """
     Raise a TypeError exception.
     """
     try:
-        1 + '1'
+        raise TypeError
     except TypeError as err:
         raise err
